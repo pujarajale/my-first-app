089173c7b0d1bbb8d7bcfa9f657a11cb86ee7f15
pujarajale <rajalepuja1997@gmail.com>
2025-07-18 21:54:17 +0530

    useState hook implemented

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -2,28 +2,33 @@ import FoodItemError from "./FoodItemError";
 import FoodItemList from "./FoodItemList";
 import CommonContainer from "./CommonContainer";
 import FoodInput from "./FoodInput";
-//  (Using Prop Concept)
+import { useState } from "react";
+
 const FoodItems = ({ foodItems }) => {
   // const foodItems = ["Veg Pizza", "Veg Burger", "Paneer Wrap", "Cold Coffee"];
+  const [textToShow, setTextToShow] = useState("Food item entered by user.");
+  //handling on click event.
   const handleBuyClick = (foodItem) => {
     console.log(`${foodItem} being bought.`);
   };
 
   const handleOnChange = (event) => {
     console.log(event.target.value);
+    setTextToShow(event.target.value);
   };
   return (
     <>
       <CommonContainer>
         <h1>Healthy Food</h1>
         <FoodInput handleChange={handleOnChange}></FoodInput>
-        <FoodItemError foodItem={foodItems}></FoodItemError>
+        <p>{textToShow}</p>
         <FoodItemList
           foodItem={foodItems}
           handleClick={(foodItem) => {
             handleBuyClick(foodItem);
           }}
         ></FoodItemList>
+        <FoodItemError foodItem={foodItems}></FoodItemError>
       </CommonContainer>
     </>
   );
